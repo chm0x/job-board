2b755d627ac9921d7f1c8c9b1b2a07249f13a8e0
Chema <chchema26@gmail.com>
2025-01-02 13:19:15 -0600

    Clean Junk code

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -20,11 +20,4 @@
             <span class="ml-2 dark:text-black"> {{ Str::ucfirst($option) }} </span>
         </label>
     @endforeach
-    {{-- @foreach (\App\Models\OfferedJob::$experience as $experience)
-        <label for="experience_{{ $experience }}" class="mb-1 flex items-center">
-            <input type="radio" name="experience" id="experience_{{ $experience }}" value="{{ $experience }}"
-                @checked($experience === request('experience')) />
-            <span class="ml-2 dark:text-black"> {{ Str::ucfirst($experience) }} </span>
-        </label>
-    @endforeach --}}
 </div>
