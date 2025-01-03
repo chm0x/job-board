c3c55f682cc1b96a7caa4df099e9425a2c1e8d21
Chema <chchema26@gmail.com>
2025-01-03 14:07:12 -0600

    Filtering Jobs: Clearing the Input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,23 @@
-<input 
+<div class="relative">
+    @if($formId)
+        <button 
+            type="button"
+            class="absolute top-0 right-0 flex h-full items-center pr-2"
+            onclick="document.getElementById('{{ $name }}').value = ''; document.getElementById('{{ $formId }}').submit()"
+        >
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="currentColor" 
+                class="size-3 text-slate-500"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>          
+        </button>
+    @endif
+    <input 
     type="text" 
     placeholder = "{{ $placeholder }}"
     value="{{ $value }}"
@@ -8,11 +27,13 @@
         w-full 
         rounded-md 
         border-0 
-        py-1.5 px-2.5 
+        py-1.5 px-2.5
+        pr-8 
         text-sm ring-1 
         ring-slate-300 
         placeholder:text-slate-400 
         focus:ring-2
         dark:text-black
         "
-/>
+    />
+</div>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,14 +3,19 @@
         'Jobs' => route('jobs.index'),
     ]" />
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     {{-- Filter Search --}}
                     <div class="mb-1 font-semibold dark:text-black">
                         Search
                     </div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                    <x-text-input 
+                        name="search" 
+                        value="{{ request('search') }}" 
+                        placeholder="Search for any text" 
+                        form-id="filtering-form"
+                    />
                 </div>
                 <div>
                     {{-- Filter Salary --}}
@@ -18,8 +23,16 @@
                         Salary
                     </div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                        <x-text-input 
+                            name="min_salary" 
+                            value="{{ request('min_salary') }}" 
+                            placeholder="From" 
+                            form-id="filtering-form"/>
+                        <x-text-input 
+                            name="max_salary" 
+                            value="{{ request('max_salary') }}" 
+                            placeholder="To" 
+                            form-id="filtering-form"/>
                     </div>
                 </div>
                 <div>
@@ -31,6 +44,7 @@
                             array_map('ucfirst', \App\Models\OfferedJob::$experience),
                             \App\Models\OfferedJob::$experience
                         )" 
+                        form-id="filtering-form"
                     />
                 </div>
                 <div>
